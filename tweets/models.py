7f6bb3d55011f65edc732096db0c12e4bb499c91
Kaous Nadir Hatem <kaous.nadir.hatem@gmail.com>
2021-07-06 03:43:48 +0100

    complete task with only django

--- tweets/models.py
+++ tweets/models.py
@@ -1,8 +1,26 @@
 from django.db import models
+import random
+
+from django.conf import settings
+
+
+
+User = settings.AUTH_USER_MODEL
 
 # Create your models here.
 
 class Tweet(models.Model):
     # id = models.AutoField(True)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
     content = models.TextField(blank=True,null=True)
     image = models.FileField(upload_to='images/',blank=True)
+
+    class Meta:
+        ordering = ['-id']
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "content": self.content,
+            "likes": random.randint(0,200)
+        }
--- twitterClone/settings.py
+++ twitterClone/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-+sc!1d#chtb!x!u)ng+lea36dkx&(*8gbdrq^lw93(ntf0hh@(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
+
+LOGIN_URL = "/login"
 
 
 # Application definition
@@ -37,8 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    # third-party
+    'rest_framework',
+    # internal
     'tweets',
+
 ]
 
 MIDDLEWARE = [
@@ -56,7 +61,7 @@ ROOT_URLCONF = 'twitterClone.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,"templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
